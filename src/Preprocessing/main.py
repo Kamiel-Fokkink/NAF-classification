9b50f27d97d738f2c92566eba8941944c669561d
jiahe.zhu0312 <jiahe.zhu0312@gmail.com>
2022-10-18 12:39:06 +0200

    uncommented lines

--- src/Preprocessing/main.py
+++ src/Preprocessing/main.py
@@ -8,17 +8,17 @@ mapping_path = "./data/naf_mapping.csv"
 
 if __name__ == "__main__":
     
-    # classes = get_naf2_to_label(mapping_path)
+    classes = get_naf2_to_label(mapping_path)
 
-    # df_train = read_naf(train_path)
-    # df_train = map_naf5_to_naf2(df_train, mapping_path)
+    df_train = read_naf(train_path)
+    df_train = map_naf5_to_naf2(df_train, mapping_path)
 
-    # df_train = apply_one_hot_encoder(df_train, list(classes.keys()))
+    df_train = apply_one_hot_encoder(df_train, list(classes.keys()))
 
     if not os.path.exists('./intermediate_outputs'):
         os.mkdir('./intermediate_outputs') 
         
-    # df_train.to_csv('./intermediate_outputs/train_onehot.csv', index=False)
+    df_train.to_csv('./intermediate_outputs/train_onehot.csv', index=False)
 
 
     # example for using tokenizer and pretrained model
