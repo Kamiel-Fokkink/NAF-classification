c61b83ffcbfd72d7ec9e56a6407898100f14f9bb
Mangqi <46937967+Mangqi@users.noreply.github.com>
2022-10-20 16:42:45 +0200

    Update main.py

--- src/Preprocessing/main.py
+++ src/Preprocessing/main.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
 
     df_train = read_naf(train_path)
     df_train = map_naf5_to_naf2(df_train, mapping_path)
+    df_train = undersample(df, threshold=1000)
     df_train = apply_clean_paragraph(df_train, rm_ponctuation=True, rm_accent=True, rm_stopword=True)
     df_train = apply_one_hot_encoder(df_train, list(classes.keys()))
 
